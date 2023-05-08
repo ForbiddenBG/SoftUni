47c1b2a85e722f9bc37a795a240bb2d4c4bcc206
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-05-08 20:07:51 +0300

    Adding Nested loops directory and exercise

---
+++ Programming_Basics/Nested_Loops/01_Clock.py
@@ -0,0 +1,11 @@
+for h in range(0, 24):
+    for m in range(0, 60):
+        print(f"{h}:{m}")
+
+#hour = 0
+#while hour <24:
+    #minutes = 0
+    #while minutes < 60
+        #print(f"{hour}:{minutes}")
+        #minutes+= 1
+    #hour += 1
