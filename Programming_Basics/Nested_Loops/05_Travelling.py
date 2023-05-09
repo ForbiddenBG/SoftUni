db3abb21595e3788f5d7b16fa54e48e90984b6ce
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-05-09 18:08:52 +0300

    Adding new nested loop exercise

---
+++ Programming_Basics/Nested_Loops/05_Travelling.py
@@ -0,0 +1,10 @@
+destination = input()
+
+while destination != "End":
+    target = float(input())
+    money_saved = 0.0
+    while money_saved < target:
+        current_money = float(input())
+        money_saved += current_money
+    print(f"Going to {destination}!")
+    destination = input()
