d6b183aeb7d99220476be371b3110dfbacdc8fcf
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-05-15 03:11:28 +0300

    Added new exercise

---
+++ More_Exercises/Training_Lab.py
@@ -0,0 +1,15 @@
+# Решението не е мое!
+
+length = float(input())
+width = float(input())
+
+desk_length = 120
+desk_width = 70
+
+hall_length = length * 100
+hall_width = (width * 100) - 100
+desks_per_l = hall_length // desk_length
+desks_per_w = hall_width // desk_width
+desks = desks_per_l * desks_per_w - 3
+
+print(desks)
