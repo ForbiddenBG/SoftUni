bab28f157b18659b4e9fdb7cc1476ee311cff56b
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-06-01 06:23:13 +0300

    Added new exercise

---
+++ Fundamentals_Module/Basic_Syntax_Conditional_Statements_and_Loops/Exercise_Basic_Syntax_Conditional_Statements_and_Loops/04_Double_Char.py
@@ -0,0 +1,18 @@
+text = input()
+
+result_text = ""
+
+for char in text:
+    # print(char * 2, end= "")
+    result_text += char * 2
+
+print(result_text)
+
+# string = input()
+#
+# double = ""
+#
+# for char in range(len(string)):
+#     double += string[char] * 2
+#
+# print(double)
