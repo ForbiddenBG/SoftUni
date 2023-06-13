2a1e11eea3b9b61c080dc0ee57becc77144d3f2c
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-06-13 08:15:44 +0300

    Added new exercise

---
+++ Fundamentals_Module/Data_Types_and_Variables/More_Exercises_Data_Types_and_Variables/01_Exchange_Integers.py
@@ -0,0 +1,22 @@
+num1 = int(input())
+num2 = int(input())
+
+print("Before:")
+print(f"a = {num1}")
+print(f"b = {num2}")
+print("After:")
+print(f"a = {num2}")
+print(f"b = {num1}")
+
+# old_a = int(input())
+# old_b = int(input())
+# temporary_a = old_a
+# new_b = temporary_a
+# temporary_b = old_b
+# new_a = temporary_b
+# print(f"Before:\n"
+#  f"a = {old_a}\n"
+#  f"b = {old_b}\n"
+#  f"After:\n"
+#  f"a = {new_a}\n"
+#  f"b = {new_b}")
