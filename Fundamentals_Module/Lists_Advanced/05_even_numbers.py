de6c7ed1cc34ef6571c3af5dc962c2b69a2b2736
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-07-15 07:56:08 +0300

    Added new exercise

---
+++ Fundamentals_Module/Lists_Advanced/05_even_numbers.py
@@ -0,0 +1,14 @@
+nums_as_string = input().split(", ")
+nums = [int(el) for el in nums_as_string]
+# nums_map = list(map(lambda el: int(el), nums_as_string))
+# nums_map_ref = list(map(int, nums_as_string))
+
+filtered_nums = [index for index in range(len(nums)) if nums[index] % 2 == 0]
+# filtered_num_filter = [nums.index(el) for el in list(filter(lambda el: el % 2 == 0, nums))]
+# filtered_num_filter = list(map(lambda el: nums.index(el), list(filter(lambda el: el % 2 == 0, nums))))
+print(filtered_nums)
+
+
+# numbers = input().split(", ")
+#
+# print([el for el in range(len(numbers)) if int(numbers[el]) % 2 == 0])
