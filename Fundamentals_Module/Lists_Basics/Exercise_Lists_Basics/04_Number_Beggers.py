b34f58a4d44790167776733cd378471e9fed3acd
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-07-20 07:55:42 +0300

    Added new exercise

---
+++ Fundamentals_Module/Lists_Basics/Exercise_Lists_Basics/04_Number_Beggers.py
@@ -0,0 +1,13 @@
+list_of_numbers_as_string = input().split(", ")
+number_of_beggars = int(input())
+sums_of_each_beggar = []
+start_index = 0
+
+for beggar in range(number_of_beggars):
+    current_sum = 0
+    for index in range(start_index, len(list_of_numbers_as_string), number_of_beggars):
+        current_sum += int(list_of_numbers_as_string[index])
+    sums_of_each_beggar.append(current_sum)
+    start_index += 1
+
+print(sums_of_each_beggar)
