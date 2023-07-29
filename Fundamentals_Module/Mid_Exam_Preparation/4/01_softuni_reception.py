5c3d2c3e2e58f9b7e06232bf0d3ffd0fb6e30c0b
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-07-29 04:11:48 +0300

    Added new exercise

---
+++ Fundamentals_Module/Mid_Exam_Preparation/4/01_softuni_reception.py
@@ -0,0 +1,32 @@
+students_per_employee_No1 = int(input())
+students_per_employee_No2 = int(input())
+students_per_employee_No3 = int(input())
+students_count = int(input())
+
+hour = 0
+
+while students_count > 0:
+    hour += 1
+    if not hour % 4 == 0:
+        students_count -= students_per_employee_No1
+        students_count -= students_per_employee_No2
+        students_count -= students_per_employee_No3
+
+print(f"Time needed: {hour}h.")
+
+
+# students_per_employee_No1 = int(input())
+# students_per_employee_No2 = int(input())
+# students_per_employee_No3 = int(input())
+# students_count = int(input())
+#
+# hour = 0
+#
+# while students_count > 0:
+#     hour += 1
+#     if hour % 4 == 0:
+#         continue
+#     else:
+#         students_count -= (students_per_employee_No1 + students_per_employee_No2 + students_per_employee_No3)
+#
+# print(f"Time needed: {hour}h.")
