9f29bc7da42c2382ba1fbc0e3056539c2f94bd8e
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-08-08 10:52:38 +0300

    Added new exercise

---
+++ Fundamentals_Module/Text_Processing/Exercise_Text_Processing/03_extract_file.py
@@ -0,0 +1,13 @@
+path = input().split("\\")
+filename, extension = path[-1].split(".")
+
+print(f"File name: {filename}")
+print(f"File extension: {extension}")
+
+# string = input().split("\\")
+#
+# file = string[-1]
+# data, temp = file.split(".")
+#
+# print(f"File name: {data}")
+# print(f"File extension: {temp}")
