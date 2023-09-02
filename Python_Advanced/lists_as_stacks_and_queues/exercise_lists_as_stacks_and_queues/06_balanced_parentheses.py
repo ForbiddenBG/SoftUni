b3dbbad04d7fc1ff15e8042a6ac1c537699cf555
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-09-02 08:54:49 +0300

    Added new exercise

---
+++ Python_Advanced/lists_as_stacks_and_queues/exercise_lists_as_stacks_and_queues/06_balanced_parentheses.py
@@ -0,0 +1,21 @@
+expression = input()
+balanced = True
+
+opening_brackets = []
+
+for ch in expression:
+    if ch in "{[(":
+        opening_brackets.append(ch)
+    elif not opening_brackets:
+        balanced = False
+        break
+    else:
+        last_opening_bracket = opening_brackets.pop()
+        if f"{last_opening_bracket}{ch}" not in "()[]{}":
+            balanced = False
+            break
+
+if balanced:
+    print("YES")
+else:
+    print("NO")
