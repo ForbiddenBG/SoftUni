983a91e3815856b817586cfcc80b57fc4937f2c5
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-09-05 03:30:35 +0300

    Added new exercise

---
+++ Python_Advanced/multidimensional_lists/04_sum_matrix_columns.py
@@ -0,0 +1,14 @@
+rows, cols = [int(x) for x in input().split(", ")]
+
+matrix = []
+
+for _ in range(rows):
+    nums = [int(el) for el in input().split()]
+    matrix.append(nums)
+
+for col_index in range(cols):
+    result = 0
+    for row_index in range(rows):
+        result += matrix[row_index][col_index]
+    print(result)
+    
