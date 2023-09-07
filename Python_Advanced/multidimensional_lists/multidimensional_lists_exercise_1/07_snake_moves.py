9bf541a69c2c9cef21285a032fefb73fa6c8947b
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-09-07 04:13:01 +0300

    Added new exercise

---
+++ Python_Advanced/multidimensional_lists/multidimensional_lists_exercise_1/07_snake_moves.py
@@ -0,0 +1,14 @@
+rows, cols = [int(x) for x in input().split()]
+
+word = input()
+index = 0
+
+for row in range(rows):
+    row_elements = []
+    for col in range(cols):
+        row_elements.append(word[index % len(word)])
+        index += 1
+    if row % 2 == 0:
+        print(*row_elements, sep="")
+    else:
+        print(*reversed(row_elements), sep="")
