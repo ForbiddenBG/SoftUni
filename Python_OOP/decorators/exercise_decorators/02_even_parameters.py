308c709cee32938e8e70900d29a5b76f07abb615
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-09-19 05:06:06 +0300

    Initial commit

---
+++ Python_OOP/decorators/exercise_decorators/02_even_parameters.py
@@ -0,0 +1,12 @@
+def even_parameters(func_ref):
+    def wrapper(*args):
+        for element in args:
+            if not isinstance(element, int) or element % 2 != 0:
+                return "Please use only even numbers!"
+        return func_ref(*args)
+    return wrapper
+
+
+@even_parameters
+def add(a, b):
+    return a + b
