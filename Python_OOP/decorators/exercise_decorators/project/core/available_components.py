58d8a8b0b9e078f8aee6ce8a28598d3c4a127a00
Trendafil Rozov <132566596+ForbiddenBG@users.noreply.github.com>
2023-09-22 04:35:10 +0300

    Initial commit

---
+++ Python_OOP/decorators/exercise_decorators/project/core/available_components.py
@@ -0,0 +1,30 @@
+desktop_available_processors = {
+    "AMD Ryzen 7 5700G": 500,
+    "Intel Core i5-12600K": 600,
+    "Apple M1 Max": 1800
+}
+
+desktop_available_rams = {
+    2: 100,
+    4: 200,
+    8: 300,
+    16: 400,
+    32: 500,
+    64: 600,
+    128: 700
+}
+
+laptop_available_processors = {
+        "AMD Ryzen 9 5950X": 900,
+        "Intel Core i9-11900H": 1050,
+        "Apple M1 Pro": 1200,
+}
+
+laptop_available_rams = {
+        2: 100,
+        4: 200,
+        8: 300,
+        16: 400,
+        32: 500,
+        64: 600,
+}
